612346aef0a9b7ff36e94fe9423ec938d6dee467
quadseed <contact@quadseed.net>
2023-04-29 22:46:16 +0900

    support responsive

--- pages/index.tsx
+++ pages/index.tsx
@@ -97,60 +97,69 @@ export default function Home() {
         <title>TARS-UI</title>
       </Head>
 
-      <main className='mx-10 my-10 space-y-10'>
-        <div className='items-center space-x-5 border-2 px-5 py-2 xl:flex xl:w-1/2'>
-          <span>更新間隔 (秒)</span>
-          <input
-            type='text'
-            ref={intervalInputRef}
-            defaultValue='30'
-            className='w-16 rounded-3xl border-2 border-indigo-200 bg-gray-100 px-5 py-0.5 outline-none focus:border-emerald-400'
-          />
-          <button
-            className='rounded-2xl border-2 border-violet-400 px-4 py-1.5'
-            onClick={() => {
-              const data: number = Number(intervalInputRef.current?.value)
-                ? Number(intervalInputRef.current?.value)
-                : 30
-              setIntervalSec(data)
-            }}
-          >
-            設定
-          </button>
-          <span className='block font-medium'>最終更新: {updateAt}</span>
+      <main className='mx-10 my-10 space-y-5'>
+        <div className='flex justify-center'>
+          <div className='w-fit items-center space-x-5 space-y-2 border-2 px-5 py-2 md:flex'>
+            <div className='space-x-3'>
+              <span>更新間隔 (秒)</span>
+              <input
+                type='text'
+                ref={intervalInputRef}
+                defaultValue='30'
+                className='w-16 rounded-3xl border-2 border-indigo-200 bg-gray-100 px-5 py-0.5 outline-none focus:border-emerald-400'
+              />
+              <button
+                className='rounded-2xl border-2 border-violet-400 px-4 py-1.5'
+                onClick={() => {
+                  const data: number = Number(intervalInputRef.current?.value)
+                    ? Number(intervalInputRef.current?.value)
+                    : 30
+                  setIntervalSec(data)
+                }}
+              >
+                設定
+              </button>
+            </div>
+            <span className='inline-block font-medium'>最終更新: {updateAt}</span>
+          </div>
         </div>
-        <div className='items-center space-x-4 py-5 max-lg:space-y-5 xl:flex xl:justify-between'>
+
+        <div className='items-center py-5 max-lg:space-y-5 xl:flex xl:justify-between'>
           <h3 className='font-bold xl:text-xl'>・録画タスク ({recordings?.length})</h3>
-          <div className='flex space-x-5'>
-            <input
-              type='text'
-              ref={recordingInputRef}
-              className='rounded-3xl border-2 border-indigo-200 bg-gray-100 px-5 py-0.5 outline-none focus:border-emerald-400'
-            />
-            <button
-              className='rounded-2xl border-2 border-blue-400 px-4 py-1.5'
-              onClick={() => {
-                startRecording(endpoint, recordingInputRef.current?.value as string)
-                setRefresh(previous => !previous)
-              }}
-            >
-              開始
-            </button>
-            <button
-              className='rounded-2xl border-2 border-red-400 px-4 py-1.5'
-              onClick={() => {
-                stopRecording(endpoint, recordingInputRef.current?.value as string)
-                setRefresh(previous => !previous)
-              }}
-            >
-              終了
-            </button>
-            <button
-              className='rounded-2xl border-2 border-violet-400 px-4 py-1.5'
-              onClick={() => setRefresh(previous => !previous)}
-            >
-              更新
-            </button>
+          <div className='flex justify-center text-center'>
+            <div className='justify-center md:flex md:space-x-5'>
+              <input
+                type='text'
+                ref={recordingInputRef}
+                className='my-5 rounded-3xl border-2 border-indigo-200 bg-gray-100 px-5 py-0.5 outline-none focus:border-emerald-400'
+              />
+              <div className='flex items-center justify-center space-x-4'>
+                <button
+                  className='my-5 rounded-2xl border-2 border-blue-400 px-4 py-1.5'
+                  onClick={() => {
+                    startRecording(endpoint, recordingInputRef.current?.value as string)
+                    setRefresh(previous => !previous)
+                  }}
+                >
+                  開始
+                </button>
+                <button
+                  className='my-5 rounded-2xl border-2 border-red-400 px-4 py-1.5'
+                  onClick={() => {
+                    stopRecording(endpoint, recordingInputRef.current?.value as string)
+                    setRefresh(previous => !previous)
+                  }}
+                >
+                  終了
+                </button>
+                <button
+                  className='my-5 rounded-2xl border-2 border-violet-400 px-4 py-1.5'
+                  onClick={() => setRefresh(previous => !previous)}
+                >
+                  更新
+                </button>
+              </div>
+            </div>
           </div>
         </div>
 
@@ -163,32 +172,36 @@ export default function Home() {
           </div>
         </div>
 
-        <div className='flex items-center space-x-4 py-5 xl:justify-between'>
+        <div className='items-center py-5 max-lg:space-y-5 xl:flex xl:justify-between'>
           <h3 className='font-bold xl:text-xl'>・自動録画対象 ({users?.length})</h3>
-          <div className='space-x-5'>
-            <input
-              type='text'
-              ref={userInputRef}
-              className='rounded-3xl border-2 border-indigo-200 bg-gray-100 px-5 py-0.5 outline-none focus:border-emerald-400'
-            />
-            <button
-              className='rounded-2xl border-2 border-blue-400 px-4 py-1.5'
-              onClick={() => {
-                addUser(endpoint, userInputRef.current?.value as string)
-                setRefresh(previous => !previous)
-              }}
-            >
-              追加
-            </button>
-            <button
-              className='rounded-2xl border-2 border-red-400 px-4 py-1.5'
-              onClick={() => {
-                removeUser(endpoint, userInputRef.current?.value as string)
-                setRefresh(previous => !previous)
-              }}
-            >
-              削除
-            </button>
+          <div className='flex justify-center text-center'>
+            <div className='md:flex md:space-x-5'>
+              <input
+                type='text'
+                ref={userInputRef}
+                className='my-5 rounded-3xl border-2 border-indigo-200 bg-gray-100 px-5 py-0.5 outline-none focus:border-emerald-400'
+              />
+              <div className='flex items-center justify-center space-x-4'>
+                <button
+                  className='my-5 rounded-2xl border-2 border-blue-400 px-4 py-1.5'
+                  onClick={() => {
+                    addUser(endpoint, userInputRef.current?.value as string)
+                    setRefresh(previous => !previous)
+                  }}
+                >
+                  追加
+                </button>
+                <button
+                  className='my-5 rounded-2xl border-2 border-red-400 px-4 py-1.5'
+                  onClick={() => {
+                    removeUser(endpoint, userInputRef.current?.value as string)
+                    setRefresh(previous => !previous)
+                  }}
+                >
+                  削除
+                </button>
+              </div>
+            </div>
           </div>
         </div>
 
